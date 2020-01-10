549ce7fa337d9c7919966fffa6464e234ee920a4
zhouxuming <zhouxuming@rongcloud.cn>
2020-01-10 14:11:19 +0800

    permission

--- app/src/main/java/baidumapsdk/demo/MainActivity.java
+++ app/src/main/java/baidumapsdk/demo/MainActivity.java
@@ -1,10 +1,13 @@
 package baidumapsdk.demo;
 
+import android.Manifest;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.baidu.mapapi.map.MapView;
+
+import pub.devrel.easypermissions.EasyPermissions;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,7 +20,24 @@ public class MainActivity extends AppCompatActivity {
         //获取地图控件引用
         mMapView = (MapView) findViewById(R.id.bmapView);
 
-        //TODO 需要重新申请秘钥 详见  http://lbsyun.baidu.com/index.php?title=androidsdk/guide/key  
+
+        String[] perms = {
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_NETWORK_STATE,
+                Manifest.permission.INTERNET
+        };
+        if (EasyPermissions.hasPermissions(this, perms)) {
+            // Already have permission, do the thing
+            // ...
+        } else {
+            // Do not have permissions, request them now
+            EasyPermissions.requestPermissions(this, "运行时权限请求",
+                    1, perms);
+        }
+
     }
 
     @Override
@@ -39,6 +59,14 @@ public class MainActivity extends AppCompatActivity {
         super.onDestroy();
         //在activity执行onDestroy时执行mMapView.onDestroy()，实现地图生命周期管理
         mMapView.onDestroy();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        // Forward results to EasyPermissions
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this);
     }
 
 }
