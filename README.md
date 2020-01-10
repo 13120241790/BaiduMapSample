663f83d23775609b81ff8027708054f6b07dc1b9
zhouxuming <zhouxuming@rongcloud.cn>
2020-01-10 11:54:10 +0800

    Add Baidu Map SDK   //TODO 重新申请秘钥

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # BaiduMapSample
+
+百度地图技术调研与储备
--- app/src/main/java/baidumapsdk/demo/MainActivity.java
+++ app/src/main/java/baidumapsdk/demo/MainActivity.java
@@ -4,12 +4,41 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.baidu.mapapi.map.MapView;
+
 public class MainActivity extends AppCompatActivity {
+
+    private MapView mMapView = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //获取地图控件引用
+        mMapView = (MapView) findViewById(R.id.bmapView);
 
+        //TODO 需要重新申请秘钥 详见  http://lbsyun.baidu.com/index.php?title=androidsdk/guide/key  
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //在activity执行onResume时执行mMapView. onResume ()，实现地图生命周期管理
+        mMapView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //在activity执行onPause时执行mMapView. onPause ()，实现地图生命周期管理
+        mMapView.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        //在activity执行onDestroy时执行mMapView.onDestroy()，实现地图生命周期管理
+        mMapView.onDestroy();
     }
 
 }
